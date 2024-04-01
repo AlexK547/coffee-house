78ef098f8bae3ed62531fe0a28f6a86736d60dd1
Alex Kozhenov <kozhenov1984@mail.ru>
2024-04-01 23:03:38 +0400

    feat: add routes and deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './components/home/Home'
 import Menu from './components/menu/Menu'
+import NotFound from './components/404NotFound'
+
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Link to={'/home'}>Home</Link>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <Link to={'/'}>Home</Link>
       <div></div>
       <Link to={'/menu'}>Menu</Link>
       <Routes>
-        <Route path='/home' element={<Home />}></Route>
+        <Route path='/' element={<Home />}></Route>
         <Route path='/menu' element={<Menu />}></Route>
+        <Route path='/*' element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
   )
