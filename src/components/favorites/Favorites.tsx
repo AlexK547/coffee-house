125f3a0a8b02e691204d827972fb196ac825a6ec
Alex Kozhenov <kozhenov1984@mail.ru>
2024-04-03 22:38:38 +0400

    feat: add Favorite section

---
+++ src/components/favorites/Favorites.tsx
@@ -0,0 +1,44 @@
+import "./Favorites.scss";
+import card1 from "../../assets/img/png/coffee-slider-1.png";
+import arrowLeft from "../../assets/img/svg/button-icon-dark.svg";
+
+export default function Favorites() {
+  return (
+    <section className="favorite">
+      <h2 className="favorite__title">
+        Choose your <span className="favorite__title_favorite">favorite</span>{" "}
+        coffee
+      </h2>
+      <div className="slider">
+        <div className="slider__arrow">
+          <img
+            className="slider__arrow_left"
+            src={arrowLeft}
+            alt="arrow left"
+          />
+        </div>
+        <div className="card">
+          <img className="card__img" src={card1} alt="card" />
+          <h3 className="card__title">S’mores Frappuccino</h3>
+          <p className="card__content">
+            This new drink takes an espresso and mixes it with brown sugar and
+            cinnamon before being topped with oat milk.
+          </p>
+          <p className="card__price">$5.50</p>
+        </div>
+        <div className="slider__arrow">
+          <img
+            className="slider__arrow_right"
+            src={arrowLeft}
+            alt="arrow right"
+          />
+        </div>
+      </div>
+      <div className="controls">
+        <div className="controls__item controls__item_select"></div>
+        <div className="controls__item"></div>
+        <div className="controls__item"></div>
+      </div>
+    </section>
+  );
+}
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,9 +1,11 @@
 import Enjoy from "../enjoy/Enjoy";
+import Favorites from "../favorites/Favorites";
 
 export default function Home() {
   return (
     <main>
       <Enjoy />
+      <Favorites />
     </main>
   )
 }
