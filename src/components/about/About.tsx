ea45c3410f0b9f37a756a2f5dd80c54a6177dec1
Alex Kozhenov <kozhenov1984@mail.ru>
2024-04-04 22:24:21 +0400

    feat: add About section

---
+++ src/components/about/About.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+import "./about.scss";
+
+const About: FC = () => {
+  return (
+    <section className="about">
+      <h2 className="about__title">
+        Resource is <span className="about__title_perfect">the perfect and cozy place</span> where you can enjoy a variety of
+        hot beverages, relax, catch up with friends, or get some work done.
+      </h2>
+      <div className="images">
+        <div className="images__left">
+            <div className="images__left_top"></div>
+            <div className="images__left_bottom"></div>
+        </div>
+        <div className="images__right">
+            <div className="images__right_top"></div>
+            <div className="images__right_bottom"></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
 import Enjoy from "../enjoy/Enjoy";
 import Favorites from "../favorites/Favorites";
+import About from "../about/About";
 
 const Home: FC = () => {
   return (
     <main>
       <Enjoy />
       <Favorites />
+      <About />
     </main>
   )
 }
