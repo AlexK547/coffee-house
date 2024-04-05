41bd34b3b362de3c75beded5996f50793d24775c
Alex Kozhenov <kozhenov1984@mail.ru>
2024-04-05 19:38:28 +0400

    feat: add video in Enjoy section

--- src/components/enjoy/Enjoy.tsx
+++ src/components/enjoy/Enjoy.tsx
@@ -1,21 +1,29 @@
-import { FC } from "react"
-import "./Enjoy.scss"
+import { FC } from "react";
+import "./Enjoy.scss";
+import video from "/video/video(720p).webm"
 
 const Enjoy: FC = () => {
   return (
     <section className="enjoy">
-        <div className="enjoy__content">
-            <h1 className="enjoy__title">
-                <span className="enjoy__title_enjoy">Enjoy</span> premium coffee at our charming cafe
-            </h1>
-            <p className="enjoy__text">
-                With its inviting atmosphere and delicious coffee options, the Coffee House Resource is a popular destination for coffee lovers and those seeking a warm and inviting space to enjoy their favorite beverage.
-            </p>
-            <button className="enjoy__btn" type="button">Menu</button>
-        </div>
-        
+      <video autoPlay muted loop>
+        <source src={video} type="video/webm" />
+      </video>
+      <div className="enjoy__content">
+        <h1 className="enjoy__title">
+          <span className="enjoy__title_enjoy">Enjoy</span> premium coffee at
+          our charming cafe
+        </h1>
+        <p className="enjoy__text">
+          With its inviting atmosphere and delicious coffee options, the Coffee
+          House Resource is a popular destination for coffee lovers and those
+          seeking a warm and inviting space to enjoy their favorite beverage.
+        </p>
+        <button className="enjoy__btn" type="button">
+          Menu
+        </button>
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default Enjoy;
