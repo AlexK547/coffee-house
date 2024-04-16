3a4ea4ef27626b43a48d632ba9f1fcd52e8016fc
Alex Kozhenov <kozhenov1984@mail.ru>
2024-04-17 02:44:26 +0400

    add switch in favorite coffee

--- src/components/favorites/Favorites.tsx
+++ src/components/favorites/Favorites.tsx
@@ -1,13 +1,32 @@
 import "./Favorites.scss";
-// import card1 from "/img/png/coffee-slider-1.png";
-import arrowLeft from "/img/svg/button-icon-dark.svg";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { listFavoriteCoffee } from "../../store/favoriteCoffee";
-import { getImageURL } from "../../utils/image-util";
+// import { getImageURL } from "../../utils/image-util";
 
 const Favorites: FC = () => {
-  const favoriteCoffee = listFavoriteCoffee[0];
-  console.log(favoriteCoffee)
+  const [numberCoffee, setNumberCoffee] = useState<number>(0);
+  const [currentCoffee, setCurrentCoffee] = useState(listFavoriteCoffee[numberCoffee]);
+
+  const onClickLeft = () => {
+    let number = numberCoffee - 1;
+    if (number < 0) {
+      number = listFavoriteCoffee.length - 1;
+    }
+    setTimeout(() => {
+      setNumberCoffee(number);
+      setCurrentCoffee(listFavoriteCoffee[number]);
+    }, 300)
+  }
+  const onClickRight = () => {
+    let number = numberCoffee + 1;
+    if (number > listFavoriteCoffee.length - 1) {
+      number = 0;
+    }
+    setTimeout(() => {
+      setNumberCoffee(number);
+    setCurrentCoffee(listFavoriteCoffee[number]);
+    }, 300)
+  }
 
   return (
     <section className="favorite" id="favorite">
@@ -15,31 +34,28 @@ const Favorites: FC = () => {
         Choose your <span>favorite</span> coffee
       </h2>
       <div className="slider">
-        <div className="slider__arrow">
-          <img
-            className="slider__arrow_left"
-            src={arrowLeft}
-            alt="arrow left"
-          />
+        <div className="slider__arrow" onClick={onClickLeft}>
+          <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M13.5 7H1M1 7L7 1M1 7L7 13" stroke="#403F3D" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
         </div>
         <div className="card">
-          <img className="card__img" src={getImageURL(favoriteCoffee.image)} alt="card" />
-          <h3 className="card__title">{favoriteCoffee.title}</h3>
-          <p className="card__content">{favoriteCoffee.text}</p>
-          <p className="card__price">{favoriteCoffee.price}</p>
+          {/* <img className="card__img" src={getImageURL(favoriteCoffee.image)} alt="card" /> */}
+          <img className="card__img" src={currentCoffee.image} alt="card" />
+          <h3 className="card__title">{currentCoffee.title}</h3>
+          <p className="card__content">{currentCoffee.text}</p>
+          <p className="card__price">{currentCoffee.price}</p>
         </div>
-        <div className="slider__arrow">
-          <img
-            className="slider__arrow_right"
-            src={arrowLeft}
-            alt="arrow right"
-          />
+        <div className="slider__arrow" onClick={onClickRight}>
+          <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 7H13.5M13.5 7L7.5 1M13.5 7L7.5 13" stroke="#403F3D" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
         </div>
       </div>
       <div className="controls">
-        <div className="controls__item controls__item_select"></div>
-        <div className="controls__item"></div>
-        <div className="controls__item"></div>
+        <div className={numberCoffee === 0 ? "controls__item controls__item_select" : "controls__item"}></div>
+        <div className={numberCoffee === 1 ? "controls__item controls__item_select" : "controls__item"}></div>
+        <div className={numberCoffee === 2 ? "controls__item controls__item_select" : "controls__item"}></div>
       </div>
     </section>
   );
--- src/store/favoriteCoffee.ts
+++ src/store/favoriteCoffee.ts
@@ -1,9 +1,27 @@
+import image1 from "/img/png/coffee-slider-1.png";
+import image2 from "/img/png/coffee-slider-2.png";
+import image3 from "/img/png/coffee-slider-3.png";
+
 export const listFavoriteCoffee = [
     {
         id: 1,
-        image: "coffee-slider-1.png",
+        image: image1,
         title: "S’mores Frappuccino",
         text: "This new drink takes an espresso and mixes it with brown sugar and cinnamon before being topped with oat milk.",
         price: "$5.50"
     },
+    {
+        id: 2,
+        image: image2,
+        title: "Caramel Macchiato",
+        text: "Fragrant and unique classic espresso with rich caramel-peanut syrup, with cream under whipped thick foam.",
+        price: "$5.00"
+    },
+    {
+        id: 3,
+        image: image3,
+        title: "Ice coffee",
+        text: "A popular summer drink that tones and invigorates. Prepared from coffee, milk and ice.",
+        price: "$4.50"
+    }
 ];
